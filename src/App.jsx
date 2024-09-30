1642625582d3248d73ee00a3fe28204b3e322a8f
Diego Lázaro <dlazarodecastro@gmail.com>
2024-09-30 20:01:33 +0200

    logos

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,23 @@ import AddReview from './pages/AddReview'
 import EditReviewPage from './pages/EditReviewPage'
 import ProfilePage from './pages/ProfilePage'
 import Library from './pages/Library'
-// import imgLogo from './assets/home.png'
+import imgLogo from './assets/1upLogo.svg'
 
 function App() {
   
   return (
     <>
     <NavBar />
-    <img src="" alt="LOGO" />
+    <img 
+          src={imgLogo} 
+          style={{
+            maxWidth: '100%',       
+            maxHeight: '100%',     
+            height: '100px',       
+            width: '400px',                   
+          }} 
+          alt="LOGO" 
+        />
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/store' element={<StorePage />} />

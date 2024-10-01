ab9c28f889e01dea81d79412dbd91f765e93a51b
Diego Lázaro <dlazarodecastro@gmail.com>
2024-10-01 17:32:44 +0200

    estilos botones, searchbar y funcionalidades ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,45 @@
-import axios from "axios"
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
-function SearchBar({setGameList}) {
-
-  const [search, setSearch] = useState("")
+function SearchBar({ setGameList }) {
+  const [search, setSearch] = useState("");
 
   const handleSearch = async () => {
     try {
-      const response = await axios.get(`${import.meta.env.VITE_RAWG_URL}/games${import.meta.env.VITE_RAWG_KEY}&search=${search}`)
-      console.log(response)
-      setGameList(response.data.results)
+      const response = await axios.get(
+        `${import.meta.env.VITE_RAWG_URL}/games${
+          import.meta.env.VITE_RAWG_KEY
+        }&search=${search}`
+      );
+      console.log(response);
+      setGameList(response.data.results);
       // setSearch("")
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
-    <div className="input-container" style={{marginBottom:"20px", marginTop:"20px"}}>
-    <input className="input" type="text" placeholder="search" onChange={(event) => setSearch(event.target.value)} value={search} />
-    <label htmlFor="input" className="label">Enter Your Name</label>
-    <div className="topline"></div>
-<div className="underline"></div>
-<button onClick={handleSearch}>Search</button>
+    <div
+      className="input-container"
+      style={{ marginBottom: "20px", marginTop: "20px" }}
+    >
+      <input
+        autoComplete="off"
+        id="input"
+        className="input"
+        type="text"
+        onChange={(event) => setSearch(event.target.value)}
+        value={search}
+      />
+      <label htmlFor="input" className="label">
+        Search Game
+      </label>
+      <div className="topline"></div>
+      <div className="underline"></div>
+      <button className="button3D" style={{marginTop:"20px",height:"40px",paddingTop:"8px"}} onClick={handleSearch}>Search</button>
     </div>
-
-    
-
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
